ba3d8a93354bb4f50ee379d64b2a5e9f53f37ae1
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2022-08-04 09:34:19 -0300

    feat: add hook to list git hub respositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 
@@ -8,6 +9,17 @@ const repository = {
 }
 
 export function RepositoryList(){
+    const [repositories, setRepositories] = useState([]);
+
+    //toda vez que repositories mudar a funcao do userEffect será executada por causa do [repositories]
+    //Com o array de dependencias vazio [], ele somente será chamada uma única vez
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, []);
+
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
