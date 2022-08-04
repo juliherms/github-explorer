adbe0b19edcd687a6dbcd9cdf59092e14ddb3a61
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2022-08-04 08:41:50 -0300

    feat: add config to hot reload from webpack

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 
+
+//conceito da imutabilidade
+//convencional:
+// usuarios = ['diego3g','diegosf','danilosf']
+// usuarios.push('rafacamarda')
+
+// novosUsuarios = [...usuarios, 'radacamarda'] -> isso eh uma forma de adicionar sem mudar a variavel
+
 export function Counter() {
     const  [counter, setCounter] = useState(0);
 
     function increment() {
+        //conceito e imutabilidade
         setCounter(counter + 1);
     }
 
